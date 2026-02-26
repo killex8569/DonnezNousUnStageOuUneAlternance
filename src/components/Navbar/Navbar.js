da1c631b80e63955c6b031b1c12d8ce8150d7e19
Valentin Ryckaert <v.ryckaert@eleve.leschartreux.net>
2026-02-26 20:05:58 +0100

    Change page names on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,9 +14,9 @@ function Navbar() {
       <div className='mobile-menu'>
         {/* Because here isShown = true, calling toogleMobileMenu sets isShown to false in every case */}
         <Link to="/" onClick={toggleMobileMenu}>accueil</Link>
-        <Link to="/competences" onClick={toggleMobileMenu}>nos compétences</Link>
-        <Link to="/parcours" onClick={toggleMobileMenu}>notre parcours</Link>
-        <Link to="/about" onClick={toggleMobileMenu}>à propos</Link>
+        <Link to="/competences" onClick={toggleMobileMenu}>compétences</Link>
+        <Link to="/parcours" onClick={toggleMobileMenu}>parcours</Link>
+        <Link to="/about" onClick={toggleMobileMenu}>contact</Link>
       </div>
     );
   };
@@ -30,9 +30,9 @@ function Navbar() {
         {/* Desktop Menu, which only appears on large screens */}
         <div className='menu'>
           <Link to="/" className={ActiveLink === 0 ? "active-link" : ""} onClick={() => { setActiveLink(0) }}>Accueil</Link>
-          <Link to="/competences" className={ActiveLink === 1 ? "active-link" : ""} onClick={() => { setActiveLink(1) }}>nos compétences</Link>
-          <Link to="/parcours" className={ActiveLink === 2 ? "active-link" : ""} onClick={() => { setActiveLink(2) }}>notre parcours</Link>
-          <Link to="/about" className={ActiveLink === 3 ? "active-link" : ""} onClick={() => { setActiveLink(3) }}>à propos</Link>
+          <Link to="/competences" className={ActiveLink === 1 ? "active-link" : ""} onClick={() => { setActiveLink(1) }}>compétences</Link>
+          <Link to="/parcours" className={ActiveLink === 2 ? "active-link" : ""} onClick={() => { setActiveLink(2) }}>parcours</Link>
+          <Link to="/about" className={ActiveLink === 3 ? "active-link" : ""} onClick={() => { setActiveLink(3) }}>contact</Link>
         </div>
 
         {/* This button only shows up on small screens. It is used to open the mobile menu */}

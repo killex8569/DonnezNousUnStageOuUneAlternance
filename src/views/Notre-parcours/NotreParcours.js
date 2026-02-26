7ab025b3cf8cbb9878e6e6251e3240d0499641fd
Valentin Ryckaert <v.ryckaert@eleve.leschartreux.net>
2026-02-26 19:20:15 +0100

    Feat: add Valentin timeline + Switch

--- src/views/Notre-parcours/NotreParcours.js
+++ src/views/Notre-parcours/NotreParcours.js
@@ -1,9 +1,10 @@
 import Timeline from "../../components/Timeline/Timeline";
+import {Switch, LeftContent, RightContent} from '../../components/Switch/Switch'
 import "./NotreParcours.css";
 
 function NotreParcours() {
 
-  const events = [
+  const AlexandreEvents = [
     {
       id: 1,
       title: "Baccalauréat",
@@ -34,12 +35,54 @@ function NotreParcours() {
       date: "2025 - 2026",
       description: "Formation d'ingnéieur en Cybersécurité"
     },
-  ];
+  ]
+
+  const ValentinEvents = [
+    {
+      id: 1,
+      title: "Master Ingénieur en Cybersécurité à CPE Lyon",
+      date: "En ce moment",
+      description: "Perfectionnement en cybersécurité, approfondissement des compétences en réseaux et développement, cours et travaux théoriques et pratiques, projets"
+    },
+    {
+      id: 2,
+      title: "Stage en développement web (2nd année de BTS) : Institut de Biologie et Chimie des Protéines",
+      date: "mars - avril 2025",
+      description: "stage de 8 semaines - développement d'une application web intégrant des services bioinformatiques : FastAPI (Python), Svelte (Javascript), MariaDB (SQL), Environnement Linux"
+    },
+    {
+      id: 3,
+      title: "Stage en développement web (1ère année de BTS) : Xev",
+      date: "Juin - Juillet 2024",
+      description: "stage de 6 semaines - travail sur différents projets agiles : Strapi, Angular, Shopify, Scripting en Python, ERP (Java, CSV)"
+    },
+    {
+      id: 4,
+      title: "BTS informatique (SIO) spécialité développement à l'Institution des Chartreux",
+      date: "septembre 2023 - juin 2025",
+      description: "Apprentissage des concepts fondamentaux de l'informatique, cours et travaux pratiques en salle informatique (Réseaux, Développement, Cybersécurité...), spécialité développement : Orienté Objet, développement web/logiciel/mobile, bases de données"
+    },
+    {
+      id: 5,
+      title: "Baccalauréat en Mathématiques et Informatique (NSI)",
+      date: "juin 2023",
+      description: "mention Très Bien"
+    },
+  ]
 
   return (
-    <div className="notre-parcours">
-      <Timeline events={events} />
-    </div>
+    <Switch leftTitle={"Alexandre"} rightTitle={"Valentin"}>
+      <LeftContent>
+      <div className="notre-parcours">
+        <Timeline events={AlexandreEvents} />
+      </div>
+      </LeftContent>
+      <RightContent>
+        <div className="notre-parcours">
+          <Timeline events={ValentinEvents} />
+        </div>
+      </RightContent>
+    </Switch>
   );
 }
 

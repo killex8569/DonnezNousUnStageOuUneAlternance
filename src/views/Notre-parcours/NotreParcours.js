58c03717a4a6ef41f0678746e72991232c696ba0
killex8569 <alexandreanette@gmail.com>
2026-02-27 20:43:36 +0100

    Update: Timeline

--- src/views/Notre-parcours/NotreParcours.js
+++ src/views/Notre-parcours/NotreParcours.js
@@ -14,27 +14,26 @@ function NotreParcours() {
     
     {
       id: 2,
-      title: "Stage de deuxième année",
+      title: "Stage en système et réseau (2ème année de BTS) : Advolis Orfis",
       date: "mars - avril 2025",
-      description: "Stage de 8 semaines Advolis Orfis - Mise en place d'une solution de déploiement, mise à jour de leurs systèmes de supervision, cartographie du réseau, déplacements et interventions sur site client."
+      description: "Stage de 8 semaines - Mise en place d'une solution de déploiement, mise à jour de leurs systèmes de supervision, cartographie du réseau, déplacements et interventions sur site client."
     },
     {
       id: 3,
-      title: "Stage de première année de BTS",
+      title: "Stage en système et réseau (2ème année de BTS) : Advolis Orfis",
       date: "juin - juillet 2024",
-      description: "Stage de 6 semaines Advolis Orfis - Mise en place d'une solution de supervision, déplacements et interventions sur site client, création de documentation."
+      description: "Stage de 6 semaines - Mise en place d'une solution de supervision, déplacements et interventions sur site client, création de documentation."
     },
     {
       id: 4,
       title: "BTS Informatique (SIO), spécialité Systèmes et Réseaux à l'Institution des Chartreux",
-      date: "2023 - 2025",
+      date: "septembre 2023 - juin 2025",
       description: "Apprentissage des concepts fondamentaux de l'informatique, cours et travaux pratiques en salle informatique (Réseaux, Développement, Cybersécurité...), spécialité Réseaux : mise en place d'infrastructures complètes, travaux de groupe, Active Directory, sécurisation du SI."
     },
     {
       id: 5,
-      title: "Baccalauréat spécialités scientifiques",
-      date: "2020 - 2023",
-      description: "Obtention de mon baccalauréat spécialités Mathématiques, Physique et SVT."
+      title: "Baccalauréat spécialités : Mathématiques, SVT",
+      date: "juin 2023",
     }
   ]
 
@@ -47,13 +46,13 @@ function NotreParcours() {
     },
     {
       id: 2,
-      title: "Stage en développement web (2nd année de BTS) : Institut de Biologie et Chimie des Protéines",
+      title: "Stage en développement web (2nd année de BTS) : Institut de Biologie et Chimie des Protéines (CNRS)",
       date: "mars - avril 2025",
       description: "stage de 8 semaines - développement d'une application web intégrant des services bio-informatiques : FastAPI (Python), Svelte (Javascript), MariaDB (SQL), Environnement Linux."
     },
     {
       id: 3,
-      title: "Stage en développement web (1ère année de BTS) : Xev",
+      title: "Stage en développement web (1ère année de BTS) : Xev - TPE éditeur de logiciel",
       date: "juin - juillet 2024",
       description: "stage de 6 semaines - travail sur différents projets agiles : Strapi, Angular, Shopify, Scripting en Python, ERP (Java, CSV)."
     },
@@ -65,7 +64,7 @@ function NotreParcours() {
     },
     {
       id: 5,
-      title: "Baccalauréat en Mathématiques et Informatique (NSI)",
+      title: "Baccalauréat spécialités Mathématiques et Informatique (NSI)",
       date: "juin 2023",
       description: "mention Très Bien"
     },
